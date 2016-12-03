eba1320ac0507cda266de58f97aa57525e4b6c94
benBen <benmbenjamin@gmail.com>
2016-12-03 15:00:19 -0800

    solo page

--- src/Solo.jsx
+++ src/Solo.jsx
@@ -2,8 +2,20 @@ import React, {Component} from 'react';
 
 class Solo extends Component {
   render() {
+    const content = this.props.route.content;
+
     return(
-      <div>Solo</div>
+      <div id="Solo">
+        {
+          content.embeds.map((e,i) => {
+            return(
+              <span className="block embed"
+                    dangerouslySetInnerHTML={{__html: e}}>
+              </span>
+            )
+          })
+        }
+      </div>
     )
   }
 }
--- src/seeds/solo.js
+++ src/seeds/solo.js
@@ -3,7 +3,11 @@ const view = {
   name : 'solo',
   component : 'Solo',
   content : {
-
+    embeds : [
+      '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/268310958&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
+      '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/69906987&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+      '<iframe width="560" height="315" src="https://www.youtube.com/embed/KI09yBVfHvg" frameborder="0" allowfullscreen></iframe>',
+    ],
   },
 }
 
